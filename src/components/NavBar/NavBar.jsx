81fa3726fe0538c77e978ef629724e925b64f733
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-06 03:53:36 -0500

    Restructuring Nav Bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,32 @@
-import './NavBar.css';
+"use client"
+
+import "./NavBar.css";
+import Link from "next/link";
 
 const NavBar = () => {
-    return(
-        <div className="nav_bar">
-          <div id='name' className='nav_item'><sub>_</sub>&#8457;<sub>.</sub>&#514;<sub>.</sub>l<sub>.</sub>&#8450;<sub>.</sub>o<sub>.</sub>&#8501;<sub>_</sub></div>
-          <div className='nav_end nav_item'>
-            <div><a href=''>Home</a></div>
-            <div><a href=''>Blog</a></div>
-            <div><a href=''>Contact</a></div>
-            <div><a href=''>WhoAmI</a></div>
-          </div>
+  return (
+    <div className="nav_bar">
+      <Link href="/">
+        <div id="name" className="nav_item">
+          <sub>_</sub>&#8457;<sub>.</sub>&#514;<sub>.</sub>l<sub>.</sub>&#8450;
+          <sub>.</sub>o<sub>.</sub>&#8501;<sub>_</sub>
         </div>
-    );
-}
+      </Link>
+      <ul className="nav_end nav_item">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/whoami">WhoAmI</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 export default NavBar;
