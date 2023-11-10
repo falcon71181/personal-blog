f8f65829b53308bb44546983818c3d4913d30022
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-10 16:50:17 +0530

    Tailwind CSS Config Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,39 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    fontFamily: {
-      heading: ['Monaco', 'Bitstream Vera Sans Mono', 'Lucida Console'],
-      sideheading: ['Agbalumo', 'Roboto'],
-    },
     extend: {
+      animation: {
+        'rotate': 'spin 80s linear infinite',
+        'rotate-reverse': 'spin 80s linear infinite reverse',
+      },
+      keyframes: {
+        spin: {
+          from: {
+            transform: 'rotate(0deg)',
+          },
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
+      },
       colors: {
         'saffron': '#fa4103',
         'grny': '#8cff00',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    fontFamily: {
+      heading: ['Monaco', 'Bitstream Vera Sans Mono', 'Lucida Console'],
+      content_font: ['Roboto Condensed', 'sans-serif'],
+    },
+  },
+  variants: {
+    extend: {
+      transform: ['active'],
     },
   },
   plugins: [],
-}
+};
