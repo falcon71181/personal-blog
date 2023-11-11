77e06e82e36405df48c17a80358e8f32b2c5a4ae
Abhay Thakur <48170773+falcon71181@users.noreply.github.com>
2023-11-11 15:26:20 +0530

    Ara  Ara Home Page MEME

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from 'next/image'
+import './globals.css';
+import Footer from '@/components/Footer/Footer'
 
 export default function Home() {
   return (
     <div>
-      
+      <div className='home pt-24 pl-[600px]'>
+        <img src='./pp.png'></img><span className='text-white text-4xl'>Ara Ara ... What Should I Post Here .. ?</span>
+      </div>
+      <Footer></Footer>
     </div>
   )
 }
