4a5c0afbecea8101b23ed913b3ca8fc2f23c83d9
Abhay Thakur <48170773+falcon71181@users.noreply.github.com>
2023-11-11 15:14:29 +0530

    [i] Added For Blog Post

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogPost = () => {
+  return (
+    <div>
+      <h1 className="text-white text-6xl pt-12">ggg</h1>
+    </div>
+  );
+};
+
+export default BlogPost;
