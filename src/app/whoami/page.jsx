56b854bf3c941c158e0250296f24a747bc269f43
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-11-15 20:05:07 +0530

    Separating Socials as a new component

--- src/app/whoami/page.jsx
+++ src/app/whoami/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Socials from "./socials/socials";
 import "./whoami.css";
 
 const whoami = () => {
@@ -116,104 +117,10 @@ const whoami = () => {
         <div className="whoami_name">
           ABHAY THAKUR
         </div>
-        <ul className="whoami_socials">
-        <li>
-        <Link
-              className="social_btn left-[45px] bg-cover cursor-pointer "
-              href="https://github.com/falcon71181"
-              target="_blank"
-            >
-              <Image
-                className="socials_img border-2 rounded-full p-[1px] z-[2]"
-                src="/socials/Github.png"
-                alt="Github"
-                width={30}
-                height={30}
-              />
-              <div className="social_name pl-2 font-heading">Github</div>
-            </Link>
-        </li>
-        <li>
-        <Link
-              className="social_btn left-[45px] bg-cover cursor-pointer "
-              href="https://www.instagram.com/falcon71181/"
-              target="_blank"
-            >
-              <Image
-                className="socials_img border-2 rounded-full p-[1px] z-[2]"
-                src="/socials/Instagram.png"
-                alt="Instagram"
-                width={30}
-                height={30}
-              />
-              <div className="social_name pl-2 font-heading">Instagram</div>
-            </Link>
-        </li>
-        <li>
-        <Link
-              className="social_btn left-[45px] bg-cover cursor-pointer "
-              href="https://www.linkedin.com/in/abhay-thakur-73470b287/"
-              target="_blank"
-            >
-              <Image
-                className="socials_img border-2 rounded-full p-[1px] z-[2]"
-                src="/socials/Linkedin.png"
-                alt="Linkedin"
-                width={30}
-                height={30}
-              />
-              <div className="social_name pl-2 font-heading">Linkedin</div>
-            </Link>
-        </li>
-        <li>
-        <Link
-              className="social_btn left-[45px] bg-cover cursor-pointer "
-              href="mailto:falcon_clutch71@protonmail.com"
-              target="_blank"
-            >
-              <Image
-                className="socials_img border-2 rounded-full p-[1px] z-[2]"
-                src="/socials/Mail.png"
-                alt="Mail"
-                width={30}
-                height={30}
-              />
-              <div className="social_name pl-2 font-heading">Mail</div>
-            </Link>
-        </li>
-        <li>
-        <Link
-              className="social_btn left-[45px] bg-cover cursor-pointer "
-              href="https://twitter.com/elonmusk"
-              target="_blank"
-            >
-              <Image
-                className="socials_img border-2 rounded-full p-[1px] z-[2]"
-                src="/socials/Twitter.png"
-                alt="Twitter"
-                width={30}
-                height={30}
-              />
-              <div className="social_name pl-2 font-heading">Twitter</div>
-            </Link>
-        </li>
-        <li>
-        <Link
-              className="social_btn left-[45px] bg-cover cursor-pointer "
-              href="https://app.hackthebox.com/profile/673636"
-              target="_blank"
-            >
-              <Image
-                className="socials_img border-2 rounded-full p-[1px] z-[2]"
-                src="/socials/HTB.png"
-                alt="HTB"
-                width={30}
-                height={30}
-              />
-              <div className="social_name pl-2 font-heading">HTB</div>
-            </Link>
-        </li>
-        </ul>
+        <Socials></Socials>
+        <div className="font-sub_title text-slate-300">
+        I'm a third-year college student currently pursuing my bachelor's degree in <strong className="text-white">Computer Science</strong>.
+        </div>
       </div>
     </div>
   );
