bfb338ba668e9a6b1a75175649712e0d34b49eff
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-04 10:29:24 +0530

    Made Footer Absolute Bottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center bg-black text-stone-300 h-full font-heading">
+    <div className="absolute bottom-0 w-full flex items-center justify-center bg-black text-stone-300 h-16 font-heading">
       <span className="text-white">&copy; 2023</span>
       <div className="text-white mx-2">
         <Link href="https://github.com/falcon71181" target="_blank" rel="noopener noreferrer">
