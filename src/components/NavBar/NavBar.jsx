7ce0f47fc23f442ad618fb7977578f7a5eaa87cb
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-04 09:45:57 +0530

    Nav Bar Restructuring

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,67 @@
-"use client"
+"use client";
 
-import "./NavBar.css";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-const NavBar = () => {
+const Navbar = () => {
+  
   const pathname = usePathname();
+
   return (
-    <div className="nav_bar">
+    <div className="fixed w-full flex justify-between items-center px-3 py-6">
       <Link href="/">
-        <div id="name" className="nav_item">
+        <div className="font-bold uppercase text-2xl text-saffron">
           <sub>_</sub>&#8457;<sub>.</sub>&#514;<sub>.</sub>l<sub>.</sub>&#8450;
           <sub>.</sub>o<sub>.</sub>&#8501;<sub>_</sub>
         </div>
       </Link>
-      <ul className="nav_end nav_item">
+
+      <ul className="flex items-center gap-7">
         <li>
-          <Link className={`${pathname === '/' ? 'text-grny hover:text-grny' : 'text-stone-300 hover:text-saffron'}`} href="/">Home</Link>
+          <Link
+            href="/"
+            className={`${pathname === "/" ? "text-grny" : "text-stone-300"} 
+                            hover:text-saffron`}
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link className={`${pathname === '/blog' ? 'text-grny hover:text-grny' : 'text-stone-300 hover:text-saffron'}`} href="/blog">Blog</Link>
+          <Link
+            href="/blog"
+            className={`${
+              pathname === "/blog" ? "text-grny" : "text-stone-300"
+            } 
+                            hover:text-saffron`}
+          >
+            Blog
+          </Link>
         </li>
         <li>
-          <Link className={`${pathname === '/contact' ? 'text-grny hover:text-grny' : 'text-stone-300 hover:text-saffron'}`} href="/contact">Contact</Link>
+          <Link
+            href="/contact"
+            className={`${
+              pathname === "/contact" ? "text-grny" : "text-stone-300"
+            } 
+                            hover:text-saffron`}
+          >
+            Contact
+          </Link>
         </li>
         <li>
-          <Link className={`${pathname === '/whoami' ? 'text-grny hover:text-grny' : 'text-stone-300 hover:text-saffron'}`} href="/whoami">WhoAmI</Link>
+          <Link
+            href="/whoami"
+            className={`${
+              pathname === "/whoami" ? "text-grny" : "text-stone-300"
+            } 
+                            hover:text-saffron`}
+          >
+            WhoAmI
+          </Link>
         </li>
       </ul>
     </div>
   );
 };
-export default NavBar;
+
+export default Navbar;
