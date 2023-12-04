3b1a8ee65fd47cd513792240d8a8e7f73be0957c
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-04 10:13:49 +0530

    Footer Restructured

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,17 @@
-import Link from 'next/link';
-import './Footer.css';
+import Link from "next/link";
 
 const Footer = () => {
-    return(
-        <div className='footer'>
-            © 2023<div className='text-white'>
-                <Link href='https://github.com/falcon71181' target='_blank'>falcon71181</Link>
-                </div>. All rights reserved .
-        </div>
-    );
-}
+  return (
+    <div className="border-2 border-red-500 flex items-center justify-center bg-black text-stone-300 w-full h-full font-heading">
+      <span className="text-white">&copy; 2023</span>
+      <div className="text-white mx-2">
+        <Link href="https://github.com/falcon71181" target="_blank" rel="noopener noreferrer">
+          falcon71181
+        </Link>
+      </div>
+      <span className="text-white">. All rights reserved.</span>
+    </div>
+  );
+};
 
 export default Footer;
