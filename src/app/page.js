0f54bd1056f60d5f2292d0147d0a2924c5c0f618
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-05 08:15:05 +0530

    Removed Unnecessary THings

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Footer from '@/components/Footer/Footer'
 export default function Home() {
   return (
     <div>
-      <div className='home pt-24 pl-[600px]'>
+      <div className='pt-24 pl-[600px]'>
         <img src='./pp.png'></img><span className='text-white text-4xl'>Ara Ara ... What Should I Post Here .. ?</span>
       </div>
       <Footer></Footer>
