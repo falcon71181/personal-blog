e2e6c067e131949f6cbf2394cf1a05e1c699aab6
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-05 09:23:05 +0530

    WhoAmI Page Restructured

--- src/app/whoami/page.jsx
+++ src/app/whoami/page.jsx
@@ -1,129 +1,53 @@
 "use client";
-import React, { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import Socials from "./socials/socials";
-import "./whoami.css";
+import React, { useEffect, useRef } from 'react';
+import ProfileHero from "@/components/WhoAmI/ProfileHero";
+import ProfileContacts from "@/components/WhoAmI/ProfileContacts";
+import TextScramble from '@/lib/TextScramble';
 
-const whoami = () => {
-  function handleClick(contenta) {
-    setModalOpen(true);
-    setModalComponent(content);
-  }
-  return (
-    <div className="whoami_container">
-      <div className="flex flex-col items-center justify-center flex-1 h-[100%]">
-        <div className="relative p-8">
-          <div className="relative p-10 border-2 border-dashed rounded-full group animate-rotate-reverse border-spacing-4 border-gray-400/50">
-            <button
-              className="animate-rotate left-[45px] top-0 absolute rounded-full bg-cover cursor-pointer border-2 border-gray-400/50 active:scale-95 hover:scale-95 transition-all duration-500"
-              onClick={() => handleClick(1)}
-            >
-              <Image
-                className="w-[40px] h-[40px] border hover:border-gray-400/50 transition-all duration-500 rounded-full p-[1px] z-[2]"
-                src="/skills/React.png"
-                alt="ReactJS"
-                width={40}
-                height={40}
-              />
-            </button>
+const WhoAmI = () => {
+    const textRef = useRef(null);
 
-            <button
-              className="animate-rotate right-[65px] -top-[8px] absolute rounded-full bg-cover cursor-pointer border-2 border-gray-400/50 active:scale-95 hover:scale-95 transition-all duration-500"
-              onClick={() => handleClick(2)}
-            >
-              <Image
-                className="w-[40px] h-[40px] border hover:border-gray-400/50 transition-all duration-500 rounded-full p-[1px] z-[2]"
-                src="/skills/Python.png"
-                alt="Python"
-                width={40}
-                height={40}
-              />
-            </button>
+    useEffect(() => {
+        const phrases = [
+            "- ",
+            "-----",
+            "----------------",
+            "-----------------------------------------",
+            "-------------------------------------------------------------------",
+            "--------------------------------------------------------------------------------------"
+        ];
 
-            <button
-              className="absolute transition-all duration-500 bg-cover border-2 rounded-full cursor-pointer -left-5 top-24 animate-rotate border-gray-400/50 active:scale-95 hover:scale-95"
-              onClick={() => handleClick(3)}
-            >
-              <Image
-                className="w-[40px] h-[40px] border hover:border-gray-400/50 transition-all duration-500 rounded-full p-[1px] z-[2]"
-                src="/skills/Tailwind.png"
-                alt="TailwindCSS"
-                width={40}
-                height={40}
-              />
-            </button>
+        const fx = new TextScramble(textRef.current);
 
-            <button
-              className="absolute transition-all duration-500 bg-cover border-2 rounded-full cursor-pointer animate-rotate -right-4 top-20 border-gray-400/50 active:scale-95 hover:scale-95"
-              onClick={() => handleClick(4)}
-            >
-              <Image
-                className="w-[40px] h-[40px] border hover:border-gray-400/50 transition-all duration-500 rounded-full p-[1px] z-[2]"
-                src="/skills/Java.png"
-                alt="Java"
-                width={40}
-                height={40}
-              />
-            </button>
+        let counter = 0;
+        const next = () => {
+            fx.setText(phrases[counter]).then(() => {
+                setTimeout(next, 800);
+            });
+            counter = (counter + 1) % phrases.length;
+        };
 
-            <button
-              className="absolute transition-all duration-500 bg-cover border-2 rounded-full cursor-pointer left-4 bottom-8 animate-rotate border-gray-400/50 active:scale-95 hover:scale-95"
-              onClick={() => handleClick(5)}
-            >
-              <Image
-                className="w-[40px] h-[40px] border hover:border-gray-400/50 transition-all duration-500 rounded-full p-[1px] z-[2]"
-                src="/skills/JavaScript.png"
-                alt="JavaScript"
-                width={40}
-                height={40}
-              />
-            </button>
+        next();
+    }, []);
+    
+    return (
+        <div className="flex flex-col items-center justify-center flex-1 h-[100%] pt-10 text-white">
+            <div className="mt-20 flex flex-col items-center justify-center flex-1 h-[100%]">
+                <ProfileHero className='relative p-8 z-0' />
 
-            <button
-              className="absolute transition-all duration-500 bg-cover border-2 rounded-full cursor-pointer right-1 animate-rotate bottom-10 border-gray-400/50 active:scale-95 hover:scale-95"
-              onClick={() => handleClick(6)}
-            >
-              <Image
-                className="w-[40px] h-[40px] border hover:border-gray-400/50 transition-all duration-500 rounded-full p-[1px] z-[2]"
-                src="/skills/C++.png"
-                alt="C++"
-                width={40}
-                height={40}
-              />
-            </button>
+                <div className="text-[#c1e47c] font-extrabold text-2xl font-montserrat text-profile_name mt-5">
+                    ABHAY THAKUR
+                </div>
 
-            <button
-              className="animate-rotate right-[40%] -bottom-4 absolute rounded-full bg-cover cursor-pointer border-2 border-gray-400/50 active:scale-95 hover:scale-95 transition-all duration-500"
-              onClick={() => handleClick(7)}
-            >
-              <Image
-                className="w-[40px] h-[40px] border hover:border-gray-400/50 transition-all duration-500 rounded-full p-[1px] z-[2]"
-                src="/skills/Nodejs.png"
-                alt="Nodejs"
-                width={40}
-                height={40}
-              />
-            </button>
+                <ProfileContacts className='whoami_socials flex flex-row justify-between p-2' />
 
-            <button
-              className="animate-rotate w-[200px] h-[200px] p-1 border-4 rounded-full hover:border-gray-400/50 cursor-pointer transition-all duration-500 z-0"
-              onClick={() => handleClick(0)}
-            >
-              <div className="w-full h-full rounded-full active:scale-95 hover:border-gray-400 border-2 bg-[url('/pp.png')] bg-cover transition-all duration-500" />
-            </button>
-          </div>
+                <div className="font-sub_title text-slate-100 mt-1">
+                    I'm a third-year college student currently pursuing my bachelor's degree in <strong className="text-white">Computer Science</strong>.
+                    <div className="text-center" ref={textRef}></div>
+                </div>
+            </div>
         </div>
-        <div className="whoami_name">
-          ABHAY THAKUR
-        </div>
-        <Socials></Socials>
-        <div className="font-sub_title text-slate-300">
-        I'm a third-year college student currently pursuing my bachelor's degree in <strong className="text-white">Computer Science</strong>.
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-export default whoami;
+export default WhoAmI;
