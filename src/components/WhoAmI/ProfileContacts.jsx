56b921a1b268f4f64b1efe39bf22b280a932ac6f
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-17 15:35:07 +0530

    Shifting SocialMedia Details in Config

--- src/components/WhoAmI/ProfileContacts.jsx
+++ src/components/WhoAmI/ProfileContacts.jsx
@@ -1,107 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
+import { socialMedia } from "@/utils/config";
 
 const ProfileContacts = ({ className }) => {
-    return (
-        <ul className={className}>
-            <li>
-                <Link
-                    className="group flex rounded-lg items-center p-2 m-2 bg-cover cursor-pointer hover:bg-gray-900"
-                    href="https://github.com/falcon71181"
-                    target="_blank"
-                >
-                    <Image
-                        className="border-2 rounded-full p-[1px] z-[2] group-hover:border-grny"
-                        src="/socials/Github.png"
-                        alt="Github"
-                        width={30}
-                        height={30}
-                    />
-                    <div className="pl-2 font-heading group-hover:text-saffron">GitHub</div>
-                </Link>
-            </li>
-            <li>
-                <Link
-                    className="group flex rounded-lg items-center p-2 m-2 bg-cover cursor-pointer hover:bg-gray-900"
-                    href="https://www.instagram.com/falcon71181/"
-                    target="_blank"
-                >
-                    <Image
-                        className="border-2 rounded-full p-[1px] z-[2] group-hover:border-grny"
-                        src="/socials/Instagram.png"
-                        alt="Instagram"
-                        width={30}
-                        height={30}
-                    />
-                    <div className="pl-2 font-heading group-hover:text-saffron">Instagram</div>
-                </Link>
-            </li>
-            <li>
-                <Link
-                    className="group flex rounded-lg items-center p-2 m-2 bg-cover cursor-pointer hover:bg-gray-900"
-                    href="https://www.linkedin.com/in/abhay-thakur-73470b287/"
-                    target="_blank"
-                >
-                    <Image
-                        className="border-2 rounded-full p-[1px] z-[2] group-hover:border-grny"
-                        src="/socials/Linkedin.png"
-                        alt="Linkedin"
-                        width={30}
-                        height={30}
-                    />
-                    <div className="pl-2 font-heading group-hover:text-saffron">LinkedIn</div>
-                </Link>
-            </li>
-            <li>
-                <Link
-                    className="group flex rounded-lg items-center p-2 m-2 bg-cover cursor-pointer hover:bg-gray-900"
-                    href="mailto:falcon_clutch71@protonmail.com"
-                    target="_blank"
-                >
-                    <Image
-                        className="border-2 rounded-full p-[1px] z-[2] group-hover:border-grny"
-                        src="/socials/Mail.png"
-                        alt="Mail"
-                        width={30}
-                        height={30}
-                    />
-                    <div className="pl-2 font-heading group-hover:text-saffron">Mail</div>
-                </Link>
-            </li>
-            <li>
-                <Link
-                    className="group flex rounded-lg items-center p-2 m-2 bg-cover cursor-pointer hover:bg-gray-900"
-                    href="https://twitter.com/elonmusk"
-                    target="_blank"
-                >
-                    <Image
-                        className="border-2 rounded-full p-[1px] z-[2] group-hover:border-grny"
-                        src="/socials/Twitter.png"
-                        alt="Twitter"
-                        width={30}
-                        height={30}
-                    />
-                    <div className="pl-2 font-heading group-hover:text-saffron">Twitter</div>
-                </Link>
-            </li>
-            <li>
-                <Link
-                    className="group flex rounded-lg items-center p-2 m-2 bg-cover cursor-pointer hover:bg-gray-900"
-                    href="https://app.hackthebox.com/profile/673636"
-                    target="_blank"
-                >
-                    <Image
-                        className="border-2 rounded-full p-[1px] z-[2] group-hover:border-grny"
-                        src="/socials/HTB.png"
-                        alt="HTB"
-                        width={30}
-                        height={30}
-                    />
-                    <div className="pl-2 font-heading group-hover:text-saffron">HTB</div>
-                </Link>
-            </li>
-        </ul>
-    )
-}
+  return (
+    <ul className={className}>
+      {socialMedia.map((item) => (
+        <li key={item.name}>
+          <Link
+            className="group flex rounded-lg items-center p-2 m-2 bg-cover cursor-pointer hover:bg-gray-900"
+            href={item.url}
+            target="_blank"
+          >
+            <Image
+              className="border-2 rounded-full p-[1px] z-[2] group-hover:border-grny"
+              src={item.img}
+              alt={item.name}
+              width={30}
+              height={30}
+            />
+            <div className="pl-2 font-heading group-hover:text-saffron">
+              {item.name}
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default ProfileContacts;
+
---
+++ src/utils/config.js
@@ -0,0 +1,34 @@
+export const email = "falcon_clutch71@gmail.com";
+
+export const socialMedia = [
+  {
+    name: "GitHub",
+    url: "https://github.com/falcon71181",
+    img: "/socials/Github.png",
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/falcon71181/",
+    img: "/socials/Instagram.png",
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/abhay-thakur-73470b287/",
+    img: "/socials/Linkedin.png",
+  },
+  {
+    name: "Mail",
+    url: "mailto:falcon_clutch71@protonmail.com",
+    img: "/socials/Mail.png",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/elonmusk",
+    img: "/socials/Twitter.png",
+  },
+  {
+    name: "HTB",
+    url: "https://app.hackthebox.com/profile/673636",
+    img: "/socials/HTB.png",
+  },
+];
