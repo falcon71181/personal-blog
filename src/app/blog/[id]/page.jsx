40e6c484654e5d53e0a4fe8bd343b9d94500e05c
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-02-03 19:57:44 +0530

    blog/:[id] Linked

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import {
+  getSortedPosts,
+  getAllPostIds,
+  getPostMetadata,
+  getPostContent,
+} from "@/utils/blog";
 
-const BlogPost = () => {
+const BlogPost = ({ params }) => {
+  const postMetaData = getPostMetadata(params.id);
   return (
-    <div>
-      <h1 className="text-white text-6xl pt-12">ggg</h1>
+    <div className="pt-20 w-full min-h-[88vh] flex justify-center">
+      <div className="mt-5 flex flex-col gap-3 items-center h-full w-[60vw] border-2 border-blue-500">
+        <h1 className="text-[2.5rem] font-extrabold text-[#ededef]">
+          {postMetaData.title}
+        </h1>
+        <div className="ml-5 w-full text-stone-300">
+          <Date>{postMetaData.date}</Date>
+        </div>
+      </div>
     </div>
   );
 };
